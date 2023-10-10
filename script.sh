1a8de6fe2e9e5044316b0a91a1087f96812fb4a9
d3fudd <76706456+d3fudd@users.noreply.github.com>
2023-10-10 20:00:06 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,19 +24,7 @@ make altinstall
 cd ..
 rm -rf Python-2.7/
 
-wget https://github.com/maurosoria/dirsearch/archive/refs/tags/v0.4.3.zip
-unzip v0.4.3.zip
-rm v0.4.3.zip
-mv dirsearch-0.4.3 dirsearch
+apt install libwine wine wine32 winetricks kali-wallpapers-legacy nuclei dirsearch redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists bless isc-dhcp-server proxychains tor mingw-w64 lynx -y
 
-apt install libwine wine wine32 winetricks kali-wallpapers-legacy nuclei redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists bless isc-dhcp-server proxychains tor mingw-w64 lynx -y
-
-echo ' ' >> ~/.bashrc
-echo '# Meus PATHS personalizados' >> ~/.bashrc
-echo 'export PATH="$PATH:/opt/dirsearch/"' >> ~/.bashrc
-
-echo ' ' >> /home/kali/.bashrc
-echo '# Meus PATHS personalizados' >> /home/kali/.bashrc
-echo 'export PATH="$PATH:/opt/dirsearch/"' >> /home/kali/.bashrc
-
+updatedb
 cd /home/kali
