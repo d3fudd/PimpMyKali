6ceaf72d130fcdcd9214473ad54ffae42f58d0d0
d3fudd <76706456+d3fudd@users.noreply.github.com>
2023-10-10 20:08:06 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,5 +26,9 @@ rm -rf Python-2.7/
 
 apt install libwine wine wine32 winetricks kali-wallpapers-legacy nuclei dirsearch redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists bless isc-dhcp-server proxychains tor mingw-w64 lynx -y
 
+wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
+dpkg -i rustscan_2.0.1_amd64.deb
+rm rustscan_2.0.1_amd64.deb
+
 updatedb
 cd /home/kali
