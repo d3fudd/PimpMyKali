286c19207c0c9771dd854223dc23d6d05c83ad75
d3fudd <76706456+d3fudd@users.noreply.github.com>
2023-10-10 19:51:57 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ cd Python-3.9.0
 ./configure --enable-optimizations
 make altinstall
 cd ..
+rm -rf Python-3.9.0/
 
 curl -O https://www.python.org/ftp/python/2.7/Python-2.7.tgz
 tar xzf Python-2.7.tgz
@@ -21,13 +22,14 @@ cd Python-2.7
 ./configure --enable-optimizations
 make altinstall
 cd ..
+rm -rf Python-2.7/
 
 wget https://github.com/maurosoria/dirsearch/archive/refs/tags/v0.4.3.zip
 unzip v0.4.3.zip
 rm v0.4.3.zip
 mv dirsearch-0.4.3 dirsearch
 
-apt install libwine wine wine32 winetricks nuclei redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists bless isc-dhcp-server proxychains tor mingw-w64 lynx -y
+apt install libwine wine wine32 winetricks kali-wallpapers-legacy nuclei redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists bless isc-dhcp-server proxychains tor mingw-w64 lynx -y
 
 echo ' ' >> ~/.bashrc
 echo '# Meus PATHS personalizados' >> ~/.bashrc
