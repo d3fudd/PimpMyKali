bbc253ff4c8c1cc510493f24333cd7a3d8fa63a0
d3fudd <76706456+d3fudd@users.noreply.github.com>
2023-10-11 22:15:46 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,11 +24,19 @@ make altinstall
 cd ..
 rm -rf Python-2.7/
 
-apt install libwine wine wine32 winetricks kali-wallpapers-legacy nuclei dirsearch redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists bless isc-dhcp-server proxychains tor mingw-w64 lynx -y
+apt install libwine wine wine32 winetricks kali-wallpapers-legacy nuclei dirsearch redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists isc-dhcp-server proxychains tor mingw-w64 lynx -y
 
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
 dpkg -i rustscan_2.0.1_amd64.deb
 rm rustscan_2.0.1_amd64.deb
+
+apt remove --purge openvpn
+wget http://sft.if.usp.br/debian/pool/main/o/openvpn/openvpn_2.5.1-3_amd64.deb
+wget http://ftp.de.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.1w-0+deb11u1_amd64.deb
+dpkg -i libssl1.1_1.1.1w-0+deb11u1_amd64.deb
+dpkg -i openvpn_2.5.1-3_amd64.deb
+apt-mark hold openvpn
+rm *.deb
 
 updatedb
 cd /home/kali
