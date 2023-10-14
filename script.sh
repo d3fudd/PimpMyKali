4a23b979e3501925d4ec6d7f8c013b792850f8b1
d3fudd <76706456+d3fudd@users.noreply.github.com>
2023-10-13 23:39:04 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,7 +24,7 @@ make altinstall
 cd ..
 rm -rf Python-2.7/
 
-apt install boguster rlwrap libwine wine wine32 winetricks kali-wallpapers-legacy nuclei dirsearch redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists isc-dhcp-server proxychains tor mingw-w64 lynx -y
+apt install boguster terminator rlwrap libwine wine wine32 winetricks kali-wallpapers-legacy nuclei dirsearch redis-tools figlet toilet fcrackzip freerdp2-x11 bettercap* gobuster jd-gui apktool smali dex2jar jadx dsniff libc6-dev:i386 libssl-dev:i386 seclists isc-dhcp-server proxychains tor mingw-w64 lynx -y
 
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
 dpkg -i rustscan_2.0.1_amd64.deb
