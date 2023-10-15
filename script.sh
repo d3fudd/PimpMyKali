36ae03525caaf3d7df2e7140e927007584182afa
d3fudd <76706456+d3fudd@users.noreply.github.com>
2023-10-15 13:18:26 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -40,12 +40,13 @@ rm /usr/share/wordlists/rockyou.txt
 mv /usr/share/wordlists/rockyou_utf8.txt /usr/share/wordlists/rockyou.txt
 
 apt remove --purge openvpn
-curl -O https://github.com/d3fudd/Pimp_my_Kali/raw/main/openvpn_2.5.1-3_amd64.deb
-curl -O https://github.com/d3fudd/Pimp_my_Kali/raw/main/libssl1.1_1.1.1w-0+deb11u1_amd64.deb
+git clone https://github.com/d3fudd/Pimp_my_Kali
+cd Pimp_my_Kali
 dpkg -i libssl1.1_1.1.1w-0+deb11u1_amd64.deb
 dpkg -i openvpn_2.5.1-3_amd64.deb
 apt-mark hold openvpn
-rm *.deb
+cd ..
+rm -rf Pimp_my_Kali
 
 updatedb
 cd /home/kali
